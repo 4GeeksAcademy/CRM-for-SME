448cbbd51309b1e03f4a3e789aa76e81730d7368
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2024-02-01 03:47:12 +0000

    modaladdclient,useinfo

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3 text-center justify-content-center d-md-flex ">
+	
+	<div className="card border-dark mb-3" style={{width: "50rem"}}>
+    	<div className="card-body">
+        	<center>
+         		4geeks LATAM Developers 2024 
+        	</center>
+        </div>
+	</div>
 	</footer>
 );
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,8 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import {ModalAddClient} from"./component/modalAddClient.jsx";
+import {UserInfo} from"./pages/UserInfo.jsx";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +31,8 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<ModalAddClient />} path="/modal" />
+                        <Route element={<UserInfo />} path="/userinfo" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
