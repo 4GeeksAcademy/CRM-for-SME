58ad98efd7fa2ecabff8e6f3d79c730c33947756
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2024-02-05 07:02:08 +0000

    Maquetado edit Notes

---
+++ src/front/js/component/ModalEditNotes.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
+
+export const ModalEditNotes = props => {
+    const [state, setState] = useState("");
+    const [inputEditTask, setInputEditTask] = useState("");
+    const { store, actions } = useContext(Context);
+
+    return (
+        <div className="modal bg-secondary py-5" tabIndex="-1" role="dialog" style={{ display: props.show ? "inline-block" : "none" }}>
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Edit Note</h5>
+                        {props.onClose ? (
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                                onClick={() => props.onClose()}
+                            ></button>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div className="modal-body">
+                        <textarea
+                            type="text"
+                            className="form-control mb-1 border border-secondary"
+                            placeholder="Eddit Note"
+                            onChange={e => setInputEditTask(e.target.value)}
+                            value={inputEditTask}
+                        />
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            data-dismiss="modal"
+                            onClick={() => {
+                                props.onClose();
+                            }}>
+                            Add Note
+                        </button>
+                        <button type="button" className="btn btn-secondary" onClick={() => props.onClose()}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ModalEditNotes.propTypes = {
+    onClose: PropTypes.func,
+
+};
+
+ModalEditNotes.defaultProps = {
+    show: false,
+    onClose: null
+};
