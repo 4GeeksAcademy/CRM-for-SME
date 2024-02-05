e03a190c6d4dc06ee48e74a017d44b5451ccc836
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2024-02-05 06:41:05 +0000

    maquetado edit Task

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState} from "react";
 import { Context } from "../store/appContext";
-import { ModalAddNotes } from "../component/ModalAddNotes.jsx";
+import { ModalEditTask } from "../component/ModalEditTask.jsx";
 
 
 export const Home = () => {
@@ -12,7 +12,7 @@ export const Home = () => {
 			<button className="btn btn-secondary" onClick={() => setState(true)}>
 				test
 			</button>
-			<ModalAddNotes show={state} onClose={() => setState(false)}   />
+			<ModalEditTask show={state} onClose={() => setState(false)}   />
 		</div>
 	);
 };
