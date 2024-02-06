718abee256198e2ba23b9ddc3db6a7447f317c05
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2024-02-06 04:12:33 +0000

    Maquetado Edit payment

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState} from "react";
 import { Context } from "../store/appContext";
-import { ModalAddChargeInvoice} from "../component/ModalAddChargeInvoice.jsx";
+import { ModalEditPayment} from "../component/ModalEditPayment.jsx";
 
 
 export const Home = () => {
@@ -12,7 +12,7 @@ export const Home = () => {
 			<button className="btn btn-secondary" onClick={() => setState(true)}>
 				test
 			</button>
-			<ModalAddChargeInvoice show={state} onClose={() => setState(false)}   />
+			<ModalEditPayment show={state} onClose={() => setState(false)}   />
 		</div>
 	);
 };
