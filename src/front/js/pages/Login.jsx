ddb41d4b6df29f3ed04603de9f5c3a16809228ec
Fabián Lasso Sanint <139248863+Fabo90@users.noreply.github.com>
2024-02-09 22:21:03 +0000

    sweetAlert Login and SignUp

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -16,65 +16,64 @@ export const Login = () => {
   }
 
   return (
-  <div className="container ">
-    <div class="row">
-      <div class="col col-xs-1"> 
-    <div className="body" style={{ backgroundImage:`url(${background})`}}>
-      {store.loggedIn ? (navigate("/home")) : (
-        <div className="text-center justify-content-center d-md-flex pt-5">
-          <div className="form-signin">
-            <form className="text-center">
-              <div className="form-floating m-1">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="user"
-                  placeholder="User name"
-                  onChange={e => setInputUser(e.target.value)}
-                  value={inputUser}
-                />
-                <label htmlFor="user">User name</label>
+    <div className="container ">
+      <div class="row">
+        <div class="col col-xs-1">
+          <div className="body" style={{ backgroundImage: `url(${background})` }}>
+            {store.loggedIn ? (navigate("/home")) : (
+              <div className="text-center justify-content-center d-md-flex m-5">
+                <div className="form-signin">
+                  <form className="text-center">
+                    <div className="form-floating m-1">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="user"
+                        placeholder="User name"
+                        onChange={e => setInputUser(e.target.value)}
+                        value={inputUser}
+                      />
+                      <label htmlFor="user">User name</label>
+                    </div>
+                    <div className="form-floating m-1">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                        onChange={e => setInputPassword(e.target.value)}
+                        value={inputPassword}
+                      />
+                      <label htmlFor="password">Password</label>
+                    </div>
+                    <div>
+                      <button
+                        className="w-100 btn btn-sm btn-primary"
+                        type="button"
+                        onClick={() => {
+                          if (inputUser != "" && inputPassword != "") {
+                            save();
+                          } else {
+                            Swal.fire({
+                              icon: "error",
+                              title: "Error",
+                              text: "Please fill out all input fields",
+                            });
+                          }
+                        }}>Log in</button>
+                      <Link
+                        to={'/signup'}
+                        className="w-100 btn btn-sm btn-secondary mt-1"
+                        type="button"
+                      >Sign Up</Link>
+                    </div>
+                  </form>
+                </div>
               </div>
-              <div className="form-floating m-1">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  placeholder="Password"
-                  onChange={e => setInputPassword(e.target.value)}
-                  value={inputPassword}
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <div>
-                <button
-                  className="w-100 btn btn-sm btn-primary"
-                  type="button"
-                  onClick={() => {
-                    if (inputUser != "" && inputPassword != "") {
-                      save();
-                    } else {
-                      // alert("Please fill out all input fields");
-                      Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: "Please fill out all input fields",
-                      });
-                    }
-                  }}>Log in</button>
-                <Link
-                  to={'/signup'}
-                  className="w-100 btn btn-sm btn-secondary mt-1"
-                  type="button"
-                >Sign Up</Link>
-              </div>
-            </form>
+            )}
           </div>
         </div>
-      )}
+      </div>
     </div>
-  </div>
-  </div>
-  </div>
   );
 };
