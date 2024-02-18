f377c29626ff04fc2f4592dac7d2be5ee5a4580d
Fabián Lasso Sanint <139248863+Fabo90@users.noreply.github.com>
2024-02-18 21:54:34 +0000

    userInfo view design

--- src/front/js/pages/UserInfo.jsx
+++ src/front/js/pages/UserInfo.jsx
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
 
+
 export const UserInfo = () => {
     const navigate = useNavigate();
     const { store, actions } = useContext(Context);
@@ -20,20 +21,24 @@ export const UserInfo = () => {
         }
     }, [store.token]);
 
-
     return (
-        <div className="container-fluid">
-            <div className="card-header m-3 card text-center">
-                <h5 className="h2 fw-bold">User info</h5>
-            </div>
-
-            <div className="card-header m-3 card text-center">
-                <h4 className="fw-bold">Name</h4>
-                <p>{store.userInfo}</p>
-                <div className="d-flex justify-content-center mt-2">
-                    <Link to={'/changepassword'}>
-                        <button className="btn btn-sm btn-secondary text-truncate">Change password</button>
-                    </Link>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-lg-6">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            <h5 className="h2 fw-bold">User info</h5>
+                        </div>
+                        <div className="card-body text-center">
+                            <h4 className="fw-bold">Name</h4>
+                            <p>{store.userInfo}</p>
+                            <div className="mt-3">
+                                <Link to={'/changepassword'}>
+                                    <button className="btn btn-sm btn-secondary text-truncate">Change password</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
